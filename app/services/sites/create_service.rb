8fc4f6b8034b025a07ec20a4b1ac44ac048bc6ae
Nywton Barros <nywton.barros@gmail.com>
2019-04-04 17:25:31 -0300

    Create Service

--- app/services/sites/create_service.rb
+++ app/services/sites/create_service.rb
@@ -12,13 +12,29 @@ module Sites
       def self.fetch_site(url)
         site = Site.new(url: url)
         doc = Nokogiri::HTML(open(url))
-        hrefs = doc.css("a").map do |link|
+
+        hrefs = get_hrefs(doc)
+        hrefs.each {|link| site.tags << Tag.new(name: 'a', content: link )}
+
+        h1s = doc.css("h1").compact.uniq
+        h1s.each {|link| site.tags << Tag.new(name: 'h1', content: link )}
+
+        h2s = doc.css("h2").compact.uniq
+        h2s.each {|link| site.tags << Tag.new(name: 'h2', content: link )}
+
+        h2s = doc.css("h3").compact.uniq
+        h2s.each {|link| site.tags << Tag.new(name: 'h2', content: link )}
+
+        site.save
+        site
+      end
+
+      def get_hrefs(doc)
+        doc.css('a').map do |link|
           if (href = link.attr("href")) && !href.empty?
             href
           end
         end.compact.uniq
-        hrefs.each {|link| site.tags << Tag.new(name: 'a', content: link )}
-        site.save
       end
   end
 end
