e18f935f36185be2d7d22003fffb5c0a6a731728
Nywton Barros <nywton.barros@gmail.com>
2019-04-05 04:41:02 -0300

    root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   resources :tags
   resources :sites
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'sites#index'
 end
