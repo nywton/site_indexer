c416317f6eb1bd01df2e489b2f1396798d2282d7
Nywton Barros <nywton.barros@gmail.com>
2019-04-06 10:53:51 -0300

    Enable Cors

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,10 @@ module SiteIndexer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.middleware.use ActionDispatch::Flash
-
+    config.action_dispatch.default_headers = {
+       'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
