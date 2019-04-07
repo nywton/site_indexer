f83868eba51d97fa8bf0eba772c743dc470ccd5b
Nywton Barros <nywton.barros@gmail.com>
2019-04-07 03:57:31 -0300

    Create just if not exists site

--- app/services/sites/create_service.rb
+++ app/services/sites/create_service.rb
@@ -10,25 +10,28 @@ module Sites
 
     private
       def self.fetch_site(url)
-        site = Site.find_or_initialize(url: url)
-	site.save
-        doc = Nokogiri::HTML.parse(open(url))
+        site = Site.find_or_initialize_by(url: url)
 
-        hrefs = doc.css("a").map do |link|
-          if (href = link.attr("href")) && !href.empty?
-            href
-          end
-        end.compact.uniq
-        hrefs.each {|link| Tag.create(name: 'a', content: link, site: site )}
+        if site.id.nil?
+          site.save
+          doc = Nokogiri::HTML.parse(open(url))
 
-        h1s = doc.css("h1").map {|h1| h1}.compact.uniq
-        h1s.each {|link| Tag.create(name: 'h1', content: link, site: site )}
+          hrefs = doc.css("a").map do |link|
+            if (href = link.attr("href")) && !href.empty?
+              href
+            end
+          end.compact.uniq
+          hrefs.each {|link| Tag.create(name: 'a', content: link, site: site )}
 
-        h2s = doc.css("h2").map {|h2| h2}.compact.uniq
-        h2s.each {|link| Tag.create(name: 'h2', content: link, site: site )}
+          h1s = doc.css("h1").map {|h1| h1}.compact.uniq
+          h1s.each {|link| Tag.create(name: 'h1', content: link, site: site )}
 
-        h2s = doc.css("h3").map {|h3| h3}.compact.uniq
-        h2s.each {|link| Tag.create(name: 'h3', content: link, site: site )}
+          h2s = doc.css("h2").map {|h2| h2}.compact.uniq
+          h2s.each {|link| Tag.create(name: 'h2', content: link, site: site )}
+
+          h3s = doc.css("h3").map {|h3| h3}.compact.uniq
+          h3s.each {|link| Tag.create(name: 'h3', content: link, site: site )}
+        end
 
         site
       end
